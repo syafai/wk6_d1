59a08b85378d9735008f4c62910a2e8339d42b49
Saraa Yafai <yafaisaraa@gmail.com>
2023-06-29 11:03:20 -0400

    added user table and sql

--- car_inventory/__init__.py
+++ car_inventory/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from config import Config
 from .site.routes import site
+from .authentication.routes import auth
+from .models import db as root_db
+
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -10,6 +14,9 @@ app = Flask(__name__)
 #     return "<p>Hello, World!</p>"
 
 app.register_blueprint(site)
+app.register_blueprint(auth)
 
 app.config.from_object(Config)
 
+root_db.init_app(app)
+migrate = Migrate(app, root_db)
--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
     """
@@ -9,6 +12,9 @@ class Config():
         create the config variables if not already not.
     """
 
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'YOU WILL NEVER GUESS THIS, MOFO'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
